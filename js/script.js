d608fa3adbdca4861a7bfdfa4f248a0bee00047a
moidodyr <khudyakov_ek@mail.ru>
2021-01-30 13:12:56 +0300

    ci(script.js):lesson02

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+const money = 15,
+    income = 'фриланс',
+    addExpenses = 'Комуналка, интернет, такси',
+    deposit = true,
+    mission = 30,
+    period = 6,
+    budgetDay = money / 30;
 
 
-alert('Привет js');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
 
-console.log('test');
+console.log('Период равен ' + period + ' месяцев.');
+console.log('Цель заработать ' + mission + ' рублей.');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
