be840d42bd8d8cd87b6fbadb2987cbb489836c21
moidodyr <khudyakov_ek@mail.ru>
2021-01-30 22:03:41 +0300

    ci(script.js):chenged output

--- js/script.js
+++ js/script.js
@@ -7,9 +7,7 @@ const money = 15,
     budgetDay = money / 30;
 
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
+console.log(typeof(money) + ', ' + typeof(income) + ', ' + typeof(deposit));
 console.log(addExpenses.length);
 
 console.log('Период равен ' + period + ' месяцев.');
